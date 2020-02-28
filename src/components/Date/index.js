1f88d6abb80819c2001eabcb0e374cf97a627cbc
Crysis020304050 <crysiswarhead020304050@gmail.com>
2020-02-28 23:20:55 +0200

    Fix logic of adding events to date

--- src/components/Date/index.js
+++ src/components/Date/index.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './Date.module.scss';
-import Moment from 'moment';
+import moment from 'moment';
 
 const EventItem = props => {
-  const { event: { isIn } } = props;
-  return <li className={classNames(styles.event, { [styles.inEvent]: isIn })}/>;
+  const { event: { isIn }, date, today } = props;
+  return <li className={classNames(styles.event, { [styles.inEvent]: isIn && date.isSameOrAfter(today.format('MM-DD-YYYY'))})}/>;
 };
 
 class Date extends Component {
 
-  renderEvents = (events) => {
+  renderEvents = (events, date, today) => {
     if (events) {
       return events.map((event, index) => {
-        if (index < 2) {
-          return <EventItem event={event}/>;
+        if (index < 3) {
+          return <EventItem event={event} date={date} today={today}/>;
         }
         return null;
       });
@@ -34,11 +34,9 @@ class Date extends Component {
     return (
       <div onClick={this.onDateClick} className={classNames(styles.container,
                                  { [styles.selected]: selectedDay.isSame(date, "date") })}>
-        <h5 className={classNames({[styles.currentDay]: today.isSame(date, "date")})}>
-
-        </h5>
+        <h5 className={classNames({[styles.currentDay]: today.isSame(date, "date")})}>{date.format('D')}</h5>
         <ul className={classNames(styles.eventList)}>
-          {this.renderEvents(events)}
+          {this.renderEvents(events, date, today)}
         </ul>
       </div>
     );
@@ -48,9 +46,9 @@ class Date extends Component {
 export default Date;
 
 Date.propTypes = {
-  date: PropTypes.instanceOf(Moment).isRequired,
-  today: PropTypes.instanceOf(Moment).isRequired,
-  selectedDay: PropTypes.instanceOf(Moment).isRequired,
+  date: PropTypes.instanceOf(moment).isRequired,
+  today: PropTypes.instanceOf(moment).isRequired,
+  selectedDay: PropTypes.instanceOf(moment).isRequired,
   selectDay: PropTypes.func.isRequired,
   events: PropTypes.array.isRequired,
 };
